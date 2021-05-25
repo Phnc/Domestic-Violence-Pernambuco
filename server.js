dcbb4e9468987000292f07848023ef2b16f0c2e2
Paulo Cavalcanti <paulohn27@hotmail.com>
2021-05-24 23:09:09 -0300

    updated app listen

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const express = require('express');
 const app   = express();
 
 const port = process.env.PORT || 8000;
+const host = '0.0.0.0';
 
 require('./app/routes')(app);
 
-app.listen(port, () => {
+app.get('/', (req, res) => {
+    res.send("We could not find this route :(");
+});
+
+app.listen(port, host, () => {
     console.log("We are live on " + port);
 });
